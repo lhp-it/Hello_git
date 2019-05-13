77e55f929c2465b4e573e559b4360ca92e095b2a
lhp-it <50547513+lhp-it@users.noreply.github.com>
2019-05-13 11:19:56 +0800

    Update Test.java

--- Hello_git/src/main/java/com/qst/demo/Test.java
+++ Hello_git/src/main/java/com/qst/demo/Test.java
@@ -2,6 +2,6 @@ package com.qst.demo;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Hello git,I come from IDEA");
+        System.out.println("HelloIDEA,I come from git");
     }
 }
