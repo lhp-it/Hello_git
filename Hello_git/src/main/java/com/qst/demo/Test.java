ff0d04ea2d6e26009dd22685029030c916c5ff80
lhp <2221213632@qq.com>
2019-05-13 13:31:28 +0800

    Second commit

--- Hello_git/src/main/java/com/qst/demo/Test.java
+++ Hello_git/src/main/java/com/qst/demo/Test.java
@@ -3,5 +3,6 @@ package com.qst.demo;
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hello git,I come from IDEA");
+        System.out.println("Nice to meet you!");
     }
 }
